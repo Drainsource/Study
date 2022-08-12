e03feb5d630880412478eaa424f28573a24efbd5
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-12 19:04:22 +0200

    StringVariable

---
+++ StringVariablesApp/StringVariables/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+string firstName = string.Empty;
+string lastName = string.Empty;
+string filePath = string.Empty;
+
+firstName = "Mate";
+lastName = "Toth";
+filePath = "C:\\Temp\\Demo";
+filePath = @"C:\Temp\Demo";
+
+//firstName = "123";
+
+//Console.WriteLine(firstName + " " + lastName);
+
+string testString = $@"The file for {firstName} is at C:\SampleFiles";
+
+
+Console.WriteLine($"{firstName} {lastName}");
+Console.WriteLine(filePath);
+  
