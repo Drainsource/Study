7fe00119749a28c986f6e0d7803bf7baf3e9e231
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-12 18:13:27 +0200

    My second app

---
+++ MySecondProject/GreetingApp/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+// - Welcome User to App
+Console.WriteLine("Welcome to the Greeting Applipication");
+Console.WriteLine("This application was built by Mate");
+Console.WriteLine("-------------------------------------");
+Console.WriteLine();
+
+// - Ask for First Name
+Console.WriteLine("Please enter your first name");
+string? name = Console.ReadLine();
+while (name == "")
+{
+    Console.WriteLine("That is not a valid name!");
+    Console.WriteLine("Please enter your first name");
+    name = Console.ReadLine();
+}
+// - Greet user by name
+Console.WriteLine($"Hello { name }");
+Console.ReadLine();
+
