bf0ba542c7fa1c8e36daf62578470519f39aedcf
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-13 14:46:02 +0200

    BasicIf

---
+++ BasicifApp/Basicif/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+//bool isComplete = false;
+
+
+//if (isComplete)
+//{
+//    Console.WriteLine("The statment was true.");
+//}
+//else
+//{
+//    Console.WriteLine("The statement was false.");
+//    Console.WriteLine("This also run.");
+//}
+
+//Console.WriteLine("End of program.");
+
+Console.Write("What is your first name: ");
+string? firstName = Console.ReadLine();
+
+if (firstName.ToLower() == "mate")
+{
+    Console.WriteLine("Hello Mr. Toth");
+}
+else
+{
+    Console.WriteLine($"Hello {firstName}");
+}
+
+Console.WriteLine("End of program.");
+
