7967003b59faa00b11e57254dc1f05eaeb53bbf2
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-13 20:27:37 +0200

    HomeWorkStudentCheck

---
+++ HomeWorkStudentCheckApp/HomeWorkStudentCheck/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.Write("What is your name: ");
+string? name = Console.ReadLine();
+
+bool isNumber;
+int age;
+do
+{
+    Console.Write("How old are you: ");
+    string? ageText = Console.ReadLine();
+    isNumber = int.TryParse(ageText, out age);
+} while (!isNumber);
+
+if (name.ToLower() == "bob" || name.ToLower() == "sue")
+{
+    Console.WriteLine($"Hello Professor {name}");
+}
+if (isNumber && age < 21)
+{
+    Console.WriteLine($"Please wait {21 - age} to start this class");
+}
