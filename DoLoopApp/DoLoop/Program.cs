06abdab27b487f7ecfced92bcee2aee7159411fb
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-13 21:10:18 +0200

    DoLoop

---
+++ DoLoopApp/DoLoop/Program.cs
@@ -0,0 +1,29 @@
+﻿
+bool isValidAge;
+int age;
+do
+{
+	Console.WriteLine("Whait is your age: ");
+	string? ageText = Console.ReadLine();
+	isValidAge = int.TryParse(ageText, out age);
+
+	if (!isValidAge)
+	{
+		Console.WriteLine("Thats was an ivalid age.");
+	}
+
+} while (!isValidAge);
+
+Console.WriteLine($"Your age is {age}");
+
+
+
+//do
+//{
+
+//} while (true);
+
+//while (true)
+//{
+
+//}
