7e7cc94eed8b9da8a0bd572be6937a340d97185d
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-13 15:34:26 +0200

    SwitchDemo

---
+++ SwitchDemoApp/SwitchDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿string firstName = "Tim";
+
+int age = 43;
+
+switch (age)
+{
+	case >= 0 and < 18:
+		Console.WriteLine("You are a child");
+		break;
+		case >= 18 and < 66:
+		Console.WriteLine("You should have a jobb");
+		break;
+	case >= 66:
+		Console.WriteLine("Hopefully you are retired");
+		break;
+	default:
+		Console.WriteLine("Age was not in an expected range");
+		break;
+}
+
+
+
+//switch (firstName.ToLower())
+//{
+//	case "Mate" or "Merci":
+//		Console.WriteLine("Hello Mate");
+//		break;
+//	case "Fanni":
+//		Console.WriteLine("Hello Fanni");
+//		break;
+//	default:
+//		Console.WriteLine("I dont know you");
+//		break;
+//}
