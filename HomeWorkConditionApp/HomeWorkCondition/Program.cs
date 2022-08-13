798a2bb07433e1e822a7bad01e5dbbeb6a31fd1c
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-13 15:37:42 +0200

    HomeWorkCondition

---
+++ HomeWorkConditionApp/HomeWorkCondition/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("What you name: ");
+string? name = Console.ReadLine();
+
+if (name.ToLower() == "tim")
+{
+    Console.WriteLine("Hello Proff. ");
+}
+else
+{
+    Console.WriteLine("Hello student.");
+}
