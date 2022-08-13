1e34a9637fcfc13b3ac3da01436511cdc662f909
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-13 13:58:16 +0200

    DateOnlyVariables

---
+++ DateOnlyVariablesApp/DateOnlyVariables/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+DateTime today = DateTime.Now;
+
+DateOnly birthday = DateOnly.Parse("6/11/1988");
+
+Console.WriteLine(birthday.ToString("MMMM dd, yyyy"));
+
+Console.WriteLine($"Today full format: {today}");
+Console.WriteLine($"Today just date {today.Date}");
+Console.WriteLine($"Birthday full format {birthday}");
+
+
+// Practice
+
+
+
+DateTime tomorow = DateTime.Now + TimeSpan.FromDays(1);
+Console.WriteLine(tomorow);
+
+DateOnly nextYear = DateOnly.FromDateTime(DateTime.Now + TimeSpan.FromDays(365));
+Console.WriteLine(nextYear);
