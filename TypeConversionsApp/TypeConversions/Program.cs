75c32ed72c8aa45f1628ac16465118e4c95fd920
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-13 14:13:54 +0200

    TypeConversionVariable

--- TypeConversionsApp/TypeConversions/Program.cs
+++ TypeConversionsApp/TypeConversions/Program.cs
@@ -3,7 +3,10 @@
 Console.Write("What is your age: ");
 string? ageText = Console.ReadLine();
 
-int age = int.Parse(ageText);
+
+bool isValidInt = int.TryParse(ageText, out int age);
+
+Console.WriteLine($"This is valid: {isValidInt}. The number was {age}.");
 
 Console.WriteLine(age + 15);
 
