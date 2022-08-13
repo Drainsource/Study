72e1f7d6a51c6435765a7477a7ff347b4e38c1b8
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-13 13:32:48 +0200

    DateTimeVariable

---
+++ DateTimeVariablesApp/DateTimeVariables/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Globalization;
+
+DateTime today = DateTime.Now;
+
+//DateTime birthday = DateTime.Parse("1988/6/23");
+
+DateTime birthday = DateTime.ParseExact("6/11/1988", "d/MM/yyyy", CultureInfo.InvariantCulture);
+
+
+Console.WriteLine(today.ToString("MMM dd, yyyy hh:mm tt zzz"));
+
