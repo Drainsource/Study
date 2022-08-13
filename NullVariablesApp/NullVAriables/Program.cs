a96132123087ed66d8868465385d478077e54fff
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-13 13:02:34 +0200

    BoolVaraibles

---
+++ NullVariablesApp/NullVAriables/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+// null - lack of value
+
+
+// Havent asked for the age yet
+int? age = null;
+bool? birthday = null;
+double? battingAverage = null;
+string? firstName = null;
+
+
+// We have asked for the age now
+age = 0;
