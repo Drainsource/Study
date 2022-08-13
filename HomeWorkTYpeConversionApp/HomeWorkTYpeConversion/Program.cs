dcc9f3cd53de7f43c37769d0bdb5ac9bbaa41a2a
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-13 14:25:44 +0200

    HomeWorkTypeConversion

---
+++ HomeWorkTYpeConversionApp/HomeWorkTYpeConversion/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Console.Write("How old are you: ");
+string ageText = Console.ReadLine();
+
+bool isValidNumber = int.TryParse(ageText, out int age);
+
+if (isValidNumber)
+{
+    Console.WriteLine($"Your will be {age + 25} in 25 years and you were {age - 25} in 25 yeayrs ago");
+}
+else { Console.WriteLine("It is not a valid number"); }
+
+
