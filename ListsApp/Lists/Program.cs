752f3d8835b2e9ada257621aa70a1b9c01c03f23
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-14 14:31:37 +0200

    Lists

---
+++ ListsApp/Lists/Program.cs
@@ -0,0 +1,13 @@
+﻿
+List<string> firstNames = new List<string>();
+
+firstNames.Add("Tim");
+firstNames.Add("Sue");
+firstNames.Add("Bob");
+firstNames.Add("Jane");
+
+Console.WriteLine(firstNames[firstNames.Count - 1]);
+
+List<int> ages = new List<int>();
+
+
