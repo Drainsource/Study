cc27b2677d741d6720aa853679d3e6326a0f86ee
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-14 14:38:01 +0200

    HomeWorkLists

---
+++ HomeWorkListApp/HomeWorkList/Program.cs
@@ -0,0 +1,14 @@
+﻿List<string?> studentList = new List<string>();
+string? answer;
+
+
+do
+{
+	Console.WriteLine("Add a student: ");
+	studentList.Add(Console.ReadLine());
+	Console.Write("Are there more Students? type 'y' if you want to continue? ");
+	answer = Console.ReadLine(); 
+} while (answer == "y");
+
+Console.WriteLine($"The number of students you added  {studentList.Count}");
+
