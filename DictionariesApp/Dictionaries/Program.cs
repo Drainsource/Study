e6244c1c039ba46ae1b545c4c149f6e451577bfb
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-14 14:49:31 +0200

    Dictionaries

---
+++ DictionariesApp/Dictionaries/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Dictionary<string,string> lookup = new();
+
+lookup["animal"] = "Not a human";
+lookup["fish"] = "Not a human that swims";
+lookup["human"] = "us";
+
+Console.WriteLine($"The definitain of fish is {lookup["fish"]}");
+
+Dictionary<int,string> emloyees = new();
+
+emloyees[95] = "Tim Corey";
+emloyees[28] = "Sue Storm";
+
+Dictionary<string,string> lookup2 = new();
+
+
+
+
+
+
