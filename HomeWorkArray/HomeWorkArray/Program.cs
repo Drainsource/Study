17a6e9f1c9af2f842cfe2c91a527f17696f32a3a
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-14 14:22:03 +0200

    HomeWorkArry

---
+++ HomeWorkArray/HomeWorkArray/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string[] names = new string[] { "Mate", "Fanni", "Merci" };
+int nameNumber;
+bool isValidNumber;
+do
+{
+    Console.WriteLine("Choose a name 0-2: ");
+    string? nameText = Console.ReadLine(); 
+    isValidNumber = int.TryParse(nameText, out nameNumber);
+} while (!isValidNumber || !(nameNumber >= 0) || !(nameNumber <= (names.Length-1)));
+Console.WriteLine($"The name yo choose is {names[nameNumber]}");
+
+
+
+
+
+
