a33c51192d39c625c59af9f92f16556baba09fce
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-14 15:03:15 +0200

    ForLoops

---
+++ ForLoopsApp/ForLoops/Program.cs
@@ -0,0 +1,13 @@
+﻿
+List<decimal> charges = new();
+
+charges.Add(234M);
+charges.Add(2M);
+charges.Add(2344M);
+
+decimal total = 0;
+
+for (int i = 0; i < charges.Count; i++)
+{
+    total += charges[i];
+}
