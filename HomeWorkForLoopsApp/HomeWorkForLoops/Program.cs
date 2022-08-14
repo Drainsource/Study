4235b161ebc89fab5e74160923f9c5f69eb12b11
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-14 15:09:20 +0200

    HomeWorkForLoops

---
+++ HomeWorkForLoopsApp/HomeWorkForLoops/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Console.WriteLine("Please add names separeated with ','");
+string nameData = Console.ReadLine();
+
+List<string> nameList = nameData.Split(',').ToList();
+
+for (int i = 0; i < nameList.Count; i++)
+{
+    Console.WriteLine($"Hello {nameList[i]}");
+}
+
+
+
