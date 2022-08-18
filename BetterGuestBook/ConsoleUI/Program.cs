68cdec537ebc960d0f6fd9d4e5beee7ddb228150
Máté Tóth <72974915+Drainsource@users.noreply.github.com>
2022-08-18 13:39:53 +0200

    BetterGuestBook

---
+++ BetterGuestBook/ConsoleUI/Program.cs
@@ -0,0 +1,31 @@
+﻿using GuestBookLibrary.Models;
+
+
+List<GuestModel> guests = new();
+string moreMoreGuest = "";
+do
+{
+    GuestModel guest = new GuestModel();
+    Console.Write("What is your first name: ");
+    guest.FirstName = Console.ReadLine();
+
+    Console.Write("What is your last name: ");
+    guest.LastName = Console.ReadLine();
+
+    Console.Write("What message would you like to tell your host: ");
+    guest.MessageToHost = Console.ReadLine();
+
+    Console.Write("Are more guests comming (yes/no) : ");
+    moreMoreGuest = Console.ReadLine();
+
+    guests.Add(guest);
+
+} while (moreMoreGuest.ToLower() == "yes");
+
+foreach (var guest in guests)
+{
+    Console.WriteLine(guest.GuestInfo);
+}
+
+Console.ReadLine();
+
